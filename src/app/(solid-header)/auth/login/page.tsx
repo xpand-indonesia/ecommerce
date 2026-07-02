43c7f6c61e1b68e773ea6e735f03eb853a60ead0
yudijohn <yudijohn.sg@gmail.com>
2026-07-03 02:19:55 +0700

    Add register link on sign in page

--- src/app/(solid-header)/auth/login/page.tsx
+++ src/app/(solid-header)/auth/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SignInPage() {
     const submit = () => { }
@@ -25,35 +26,41 @@ export default function SignInPage() {
                             </div>
                         </div>
                     </div>
-                    <div className="flex-1 flex flex-col gap-8">
-                        <h2 className="font-heading font-bold text-3xl md:text-4xl text-dark leading-tight text-center">Login</h2>
-                        <form className="flex flex-col gap-4">
-                            <div className="w-full">
-                                <div className="flex-1 flex flex-col gap-2">
-                                    <label className="text-sm color-[#414651]">Username or Email Address</label>
-                                    <input
-                                        className="w-full px-4 bg-white border border-[#E9EAEB] rounded-full h-[51px] font-body text-base outline-none focus:border-primary transition-colors text-dark"
-                                        placeholder="olivia@untitledui.com"
-                                    />
+                    <div className="flex-1">
+                        <div className=" flex flex-col gap-8">
+                            <h2 className="font-heading font-bold text-3xl md:text-4xl text-dark leading-tight text-center">Login</h2>
+                            <form className="flex flex-col gap-4">
+                                <div className="w-full">
+                                    <div className="flex-1 flex flex-col gap-2">
+                                        <label className="text-sm color-[#414651]">Username or Email Address</label>
+                                        <input
+                                            className="w-full px-4 bg-white border border-[#E9EAEB] rounded-full h-[51px] font-body text-base outline-none focus:border-primary transition-colors text-dark"
+                                            placeholder="olivia@untitledui.com"
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="w-full">
-                                <div className="flex-1 flex flex-col gap-2">
-                                    <label className="text-sm color-[#414651]">Password</label>
-                                    <input
-                                        className="w-full px-4 bg-white border border-[#E9EAEB] rounded-full h-[51px] font-body text-base outline-none focus:border-primary transition-colors text-dark"
-                                        placeholder="Enter Password"
-                                    />
+                                <div className="w-full">
+                                    <div className="flex-1 flex flex-col gap-2">
+                                        <label className="text-sm color-[#414651]">Password</label>
+                                        <input
+                                            className="w-full px-4 bg-white border border-[#E9EAEB] rounded-full h-[51px] font-body text-base outline-none focus:border-primary transition-colors text-dark"
+                                            placeholder="Enter Password"
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <Button
-                                variant="primary"
-                                className="mt-2 h-[51px] px-8 cursor-pointer font-semibold shadow-lg shadow-black/20"
-                                onClick={submit}
-                            >
-                                Login
-                            </Button>
-                        </form>
+                                <Button
+                                    variant="primary"
+                                    className="mt-2 h-[51px] px-8 cursor-pointer font-semibold shadow-lg shadow-black/20"
+                                    onClick={submit}
+                                >
+                                    Login
+                                </Button>
+                            </form>
+                        </div>
+                        <div className="border-b border-[#D5D7DA] font-medium w-full my-4"></div>
+                        <div>
+                            Don't have an account? <Link href="/auth/register" className="text-primary">Register Now</Link>
+                        </div>
                     </div>
                 </div>
             </section>
