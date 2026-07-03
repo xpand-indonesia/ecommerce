f745444488311e30a8e27ede7bc34e9083c28ff8
yudijohn <yudijohn.sg@gmail.com>
2026-07-03 17:31:18 +0700

    Add badge component

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+
+const badgeVariants = cva(
+    'text-xs leading-none p-1 rounded-full',
+    {
+        variants: {
+            variant: {
+                default: '',
+                danger: 'bg-red-500 text-white'
+            },
+        },
+        defaultVariants: {
+            variant: 'default',
+        },
+    },
+);
+
+const Badge = ({
+    className,
+    variant,
+    ...props
+}: React.ComponentPropsWithoutRef<'span'> & VariantProps<typeof badgeVariants>) => {
+
+    return (
+        <span
+            data-slot="badge"
+            className={cn(badgeVariants({ variant }), className)}
+            {...props}
+        />
+    );
+};
+
+export {
+    Badge
+};
