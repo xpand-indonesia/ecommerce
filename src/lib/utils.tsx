76a50ddd901bd4db3ecc962569b8af78ddbb61d3
yudijohn <yudijohn.sg@gmail.com>
2026-07-03 17:30:47 +0700

    Add format number utility

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,3 +4,23 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]): string {
     return twMerge(clsx(inputs));
 }
+
+interface FormatNumberOptions {
+    locale?: string;
+    currency?: string;
+    decimalPlaces?: number;
+}
+
+export function formatNumber(
+    amount: number,
+    options: FormatNumberOptions = {}
+): string {
+    const { locale = 'id-ID', currency, decimalPlaces = 0 } = options;
+
+    return new Intl.NumberFormat(locale, {
+        style: !!currency ? 'currency' : 'decimal',
+        currency: currency,
+        minimumFractionDigits: decimalPlaces,
+        maximumFractionDigits: decimalPlaces
+    }).format(amount);
+}
