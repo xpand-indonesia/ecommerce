9568b930375cb71193ed3a921010812c20694916
yudijohn <yudijohn.sg@gmail.com>
2026-07-13 13:25:02 +0700

    Improving coffee detail information

--- src/app/features/product-detail/components/product-detail-section/product-detail-coffee-detail.tsx
+++ src/app/features/product-detail/components/product-detail-section/product-detail-coffee-detail.tsx
@@ -1,56 +1,60 @@
 import { CoffeeBean, Global, RefreshCircle } from "@/components/icons";
 import { FavoriteChart } from "@/components/icons/favorite-chart";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { ReactNode } from "react";
+
+interface CoffeeDetail {
+    id: string;
+    icon: ReactNode;
+    title: string;
+    description: string;
+};
+
+const DETAILS: CoffeeDetail[] = [
+    {
+        id: "level",
+        icon: <CoffeeBean />,
+        title: 'Roast Level',
+        description: 'Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!'
+    },
+    {
+        id: "origin",
+        icon: <Global />,
+        title: 'Origin',
+        description: 'Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!'
+    },
+    {
+        id: "variety",
+        icon: <RefreshCircle />,
+        title: 'Variety / Processing',
+        description: 'Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!'
+    },
+    {
+        id: "growing",
+        icon: <FavoriteChart />,
+        title: 'Growing Altitude',
+        description: 'Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!'
+    }
+];
 
 export const ProductDetailCofeeDetail = () => {
     return (
         <div className="space-y-6">
             <h3 className="font-heading text-lg font-bold">Coffee Details</h3>
             <Accordion defaultValue="level" type="single" className="border-t border-gray-200" collapsible>
-                <AccordionItem value="level">
-                    <AccordionTrigger>
-                        <div className="flex items-center text-gray-800 gap-1">
-                            <CoffeeBean />
-                            Roast Level
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        <p>Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!</p>
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="origin">
-                    <AccordionTrigger>
-                        <div className="flex items-center text-gray-800 gap-1">
-                            <Global />
-                            Origin
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        <p>Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!</p>
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="variety">
-                    <AccordionTrigger>
-                        <div className="flex items-center text-gray-800 gap-1">
-                            <RefreshCircle />
-                            Variety / Processing
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        <p>Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!</p>
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="growing">
-                    <AccordionTrigger>
-                        <div className="flex items-center text-gray-800 gap-1">
-                            <FavoriteChart />
-                            Growing Altitude
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        <p>Discover the essence of our coffee bean journey! Each of our charming cafes in Northwest Arkansas offers a unique experience, showcasing the rich flavors and aromas of our carefully sourced coffee beans. Check out our cafe hours, dive into our diverse menu, and plan your visit any day of the week!</p>
-                    </AccordionContent>
-                </AccordionItem>
+                {DETAILS.map((detail) => (
+                    <AccordionItem value={detail.id} key={`coffee-detail-${detail.id}`}>
+                        <AccordionTrigger>
+                            <div className="flex items-center text-gray-800 gap-1">
+                                <CoffeeBean />
+                                {detail.title}
+                            </div>
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            <p className="text-sm text-gray-800">{detail.description}</p>
+                        </AccordionContent>
+                    </AccordionItem>
+                ))}
             </Accordion>
         </div>
     );
