beccd9003a14262bad52d116ff4c1196667e3486
yudijohn <yudijohn.sg@gmail.com>
2026-08-12 19:17:04 +0700

    Add account information page

--- src/app/profile/account/page.tsx
+++ src/app/profile/account/page.tsx
@@ -4,6 +4,16 @@ export default function ProfileAccountPage() {
     return (
         <>
             <ContentTitle>Account Information</ContentTitle>
+            <div className="flex flex-col gap-10">
+                <div className="text-sm text-gray-800">
+                    You will lose all your data on our web store and will not be
+                    able to retrieve any content, such as personal information,
+                    purchase history, points, and membership.
+                </div>
+                <div className="mx-auto text-danger p-2 cursor-pointer">
+                    Remove My Account
+                </div>
+            </div>
         </>
     );
 }
