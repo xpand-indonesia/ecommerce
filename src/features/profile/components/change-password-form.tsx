204cfa1c4346bc469c9b144aac7888796ea6dda1
yudijohn <yudijohn.sg@gmail.com>
2026-08-13 10:20:30 +0700

    remove unused import

--- src/features/profile/components/change-password-form.tsx
+++ src/features/profile/components/change-password-form.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/form/field/input';
 import { Label } from '@/components/ui/form/label';
-import { ChangePasswordTrigger } from './change-password-trigger';
 import { useState } from 'react';
 
 export function ChangePasswordForm() {
