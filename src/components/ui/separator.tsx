70e94e5388cfdd59a4151d70ce22a18052698095
yudijohn <yudijohn.sg@gmail.com>
2026-08-13 11:34:53 +0700

    Add dashed style separator component

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,27 +1,58 @@
 'use client';
 
-import * as React from 'react';
-import { Separator as SeparatorPrimitive } from 'radix-ui';
 import { cn } from '@/lib/utils';
+import { Separator as SeparatorPrimitive } from 'radix-ui';
+import * as React from 'react';
+
+interface SeparatorProps extends React.ComponentProps<
+    typeof SeparatorPrimitive.Root
+> {
+    dashed?: boolean;
+    dashArray?: string;
+}
 
 function Separator({
     className,
     orientation = 'horizontal',
     decorative = true,
+    dashed,
+    dashArray = '5,5',
     ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: SeparatorProps) {
+    const isHorizontal = orientation === 'horizontal';
+
     return (
         <SeparatorPrimitive.Root
             data-slot="separator"
             decorative={decorative}
             orientation={orientation}
             className={cn(
-                'shrink-0 bg-gray-200',
+                'relative shrink-0 overflow-hidden',
+                dashed ? 'bg-transparent' : 'bg-gray-200',
                 orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
-                className,
+                className
             )}
             {...props}
-        />
+        >
+            {dashed && (
+                <svg
+                    width="100%"
+                    height="100%"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute inset-0 block overflow-visible"
+                >
+                    <line
+                        x1="0"
+                        y1="0"
+                        x2={isHorizontal ? '100%' : '0'}
+                        y2={isHorizontal ? '0' : '100%'}
+                        strokeWidth={1.5}
+                        strokeDasharray={dashArray}
+                        className={'stroke-current'}
+                    />
+                </svg>
+            )}
+        </SeparatorPrimitive.Root>
     );
 }
 
