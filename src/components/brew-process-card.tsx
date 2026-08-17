b63bf1c0f57bf6b8e22e5e38536791455621139a
yudijohn <yudijohn.sg@gmail.com>
2026-08-17 22:33:39 +0700

    Improve brew process section style in product detail page

--- src/components/brew-process-card.tsx
+++ src/components/brew-process-card.tsx
@@ -1,13 +1,23 @@
+import { cn } from '@/lib/utils';
+
 interface BrewProcessCard {
     title: string;
     processes: string[];
+    className?: string;
 }
 
-const BrewProcessCard = ({ title, processes }: BrewProcessCard) => {
+const BrewProcessCard = ({ title, processes, className }: BrewProcessCard) => {
     return (
-        <div className="bg-white/20 backdrop-blur-md text-white rounded-2xl flex flex-col gap-4 p-4 min-h-[256px]">
-            <h3 className="font-heading font-bold text-3xl">{title}</h3>
-            <ol className="list-disc pl-5">
+        <div
+            className={cn(
+                'bg-white/20 backdrop-blur-md text-white rounded-2xl flex flex-col gap-4 p-4 min-h-[143px] lg:min-h-[256px]',
+                className
+            )}
+        >
+            <h3 className="font-heading font-bold text-xl leading-[27px] lg:text-3xl lg:leading-[40px]">
+                {title}
+            </h3>
+            <ol className="list-disc pl-5 text-sm leading-[1.214285714] lg:text-base lg:leading-[1.1875]">
                 {processes.map((process, index) => (
                     <li key={`process-${index}`}>{process}</li>
                 ))}
@@ -16,6 +26,4 @@ const BrewProcessCard = ({ title, processes }: BrewProcessCard) => {
     );
 };
 
-export {
-    BrewProcessCard
-};
+export { BrewProcessCard };
--- src/features/product-detail/views/product-detail-brew-process-section-view.tsx
+++ src/features/product-detail/views/product-detail-brew-process-section-view.tsx
@@ -1,36 +1,64 @@
-import { BrewProcessCard } from "@/components/brew-process-card";
-import Container from "@/components/ui/container";
+import { BrewProcessCard } from '@/components/brew-process-card';
+import Container from '@/components/ui/container';
 
 export const ProductDetailBrewProcessSectionView = () => {
     return (
         <section className="bg-[linear-gradient(277.39deg,_#111E0B_5.08%,_#46B83D_107.32%)]">
-            <Container className="pt-[84px] pb-[92px]">
-                <div className="px-15 flex flex-col gap-[50px]">
-                    <h2 className="text-white font-heading text-5xl font-bold text-center">How to Brew</h2>
-                    <div className="grid grid-cols-5 gap-6">
+            <Container className="pt-[40px] pb-[40px] lg:pt-[84px] lg:pb-[92px]">
+                <div className="px-0 flex flex-col gap-[28.69px] lg:px-15 lg:gap-[51px]">
+                    <h2 className="text-white font-heading text-3xl font-bold text-center lg:text-5xl">
+                        How to Brew
+                    </h2>
+                    <div className="grid grid-cols-2 gap-y-6 gap-x-4 lg:gap lg:grid-cols-5 lg:gap-x-6">
                         <BrewProcessCard
                             title="Chemex"
-                            processes={['Dose: 40g', 'Grind: Medium', 'Brew Weight: 630g', 'Brew Time: 4:00']}
+                            processes={[
+                                'Dose: 40g',
+                                'Grind: Medium',
+                                'Brew Weight: 630g',
+                                'Brew Time: 4:00',
+                            ]}
                         />
                         <BrewProcessCard
                             title="V60"
-                            processes={['Dose: 30g', 'Grind: Medium-Fine', 'Brew Weight: 500g', 'Brew Time: 3:30']}
+                            processes={[
+                                'Dose: 30g',
+                                'Grind: Medium-Fine',
+                                'Brew Weight: 500g',
+                                'Brew Time: 3:30',
+                            ]}
                         />
                         <BrewProcessCard
                             title="AeroPress"
-                            processes={['Dose: 15g', 'Grind: Fine', 'Brew Weight: 250g', 'Brew Time: 2:00']}
+                            processes={[
+                                'Dose: 15g',
+                                'Grind: Fine',
+                                'Brew Weight: 250g',
+                                'Brew Time: 2:00',
+                            ]}
                         />
                         <BrewProcessCard
                             title="French Press"
-                            processes={['Dose: 50g', 'Grind: Coarse', 'Brew Weight: 800g', 'Brew Time: 4:00']}
+                            processes={[
+                                'Dose: 50g',
+                                'Grind: Coarse',
+                                'Brew Weight: 800g',
+                                'Brew Time: 4:00',
+                            ]}
                         />
                         <BrewProcessCard
                             title="Siphon"
-                            processes={['Dose: 20g', 'Grind: Medium', 'Brew Weight: 360g', 'Brew Time: 5:00']}
+                            processes={[
+                                'Dose: 20g',
+                                'Grind: Medium',
+                                'Brew Weight: 360g',
+                                'Brew Time: 5:00',
+                            ]}
+                            className="col-span-full lg:col-span-1"
                         />
                     </div>
                 </div>
             </Container>
         </section>
     );
-}
+};
